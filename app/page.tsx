06b96ee8fbc4e9fcdec353361a8d2bef658481a4
Diego <zambranobraundiego@gmail.com>
2025-12-17 14:04:56 +0100

    Fix TypeScript: use dedicated function for canValidate to ensure boolean return type

--- app/page.tsx
+++ app/page.tsx
@@ -589,13 +589,7 @@ export default function Home() {
               showValidationButton={true}
               onValidate={handleSubmit}
               isSubmitting={isSubmitting}
-              canValidate={Boolean(
-                quoteItems.length > 0 && 
-                !!clientInfo.name && 
-                !!clientInfo.email && 
-                (delivery.type !== 'livraison' || (!!delivery.address?.street && !!delivery.address?.city && !!delivery.address?.postalCode)) &&
-                (!delivery.billingAddressDifferent || (!!delivery.billingAddress?.street && !!delivery.billingAddress?.city && !!delivery.billingAddress?.postalCode))
-              )}
+              canValidate={canValidateOrder()}
             />
           ) : (
             <OrderSummary 
